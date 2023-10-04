7716df74033adb6ef0eb1dcb2b89871f514c1a26
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-10-04 19:36:04 +0300

    Task 2. From the Company Director

--- R4C/urls.py
+++ R4C/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from robots.views import RobotView
+from robots.views import RobotView, RobotLastWeekDataView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('robots/', RobotView.as_view())
+    path('robots/', RobotView.as_view()),
+    path('download/', RobotLastWeekDataView.as_view()),
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,7 +1,14 @@
 import json
+import os
 
+import pandas as pd
+
+from io import BytesIO
+from xlsxwriter import xmlwriter
+from datetime import datetime, timedelta
+from django.db.models import Count
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 
 from .models import Robot
 from .forms import RobotForm
@@ -26,10 +33,59 @@ class RobotView(View):
                     version=version,
                     created=created)
                 robot.save()
-                return HttpResponse("Your data has been saved!")
+                return HttpResponse('Your data has been saved!')
 
             else:
                 return HttpResponse(form.errors.as_json(), 500)
 
         except Exception as e:
             return HttpResponse('Incorrect input data', 500)
+
+
+class RobotLastWeekDataView(View):
+    model = Robot
+
+    def get(self, request):
+        try:
+            six_days_ago = datetime.now() - timedelta(days=6)
+            robots = (Robot.objects
+                      .filter(created__range=(six_days_ago, datetime.now()))
+                      .values('model', 'version')
+                      .annotate(total_created=Count('created'))
+                      .order_by('model', 'total_created'))
+
+            filename = 'robots_last_week.xlsx'
+
+            data = []
+            current_model = None
+
+            with BytesIO() as b:
+                with pd.ExcelWriter(b, engine="xlsxwriter") as wr:
+                    for robot in robots:
+                        if current_model == robot['model']:
+                            data.append({
+                                "Модель": robot['model'],
+                                "Версия": robot['version'],
+                                "Количество за неделю": robot['total_created']
+                            })
+                            sheet = pd.DataFrame(data)
+                            sheet.to_excel(wr, sheet_name=f"{robot['model']}", index=False)
+
+                        else:
+                            data = []
+                            data.append({
+                                "Модель": robot['model'],
+                                "Версия": robot['version'],
+                                "Количество за неделю": robot['total_created']
+                            })
+                            sheet = pd.DataFrame(data)
+                            sheet.to_excel(wr, sheet_name=f"{robot['model']}", index=False)
+                            current_model = robot['model']
+
+                response = HttpResponse(b.getvalue(), content_type='application/force-download')
+                response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+            return response
+
+        except Exception as e:
+            return HttpResponse(f'Oops, Something went wrong, [{e}]', 500)
